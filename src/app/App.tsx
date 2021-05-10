c6db58bd8a34c15d8bbd94ed0a5964bfa92e9065
Max <jmksm96@gmail.com>
2021-05-10 14:58:55 +0300

    Refactor

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,7 +4,9 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Login from '../features/login/Login';
 import Registration from '../features/registration/Registration'
 import Header from '../components/Header/Header';
-// import ForgotPasswordForm from "../features/forgot-password/ForgotPasswordForm";
+import ForgotPasswordForm from "../features/forgot-password/ForgotPasswordForm";
+import NewPassword from "../features/new-password/NewPassword";
+
 
 const App = () => {
   return (
@@ -16,8 +18,8 @@ const App = () => {
           <Route path='/login' render={() => <Login />}/>
           <Route path='/registration' render={() => <Registration />}/>
           <Route path='/profile' render={() => <div>Profile will be here</div>}/>
-          {/* <Route path='/forgotpassword' render={() => <div><ForgotPasswordForm/></div>}/> */}
-          <Route path='/newpassword' render={() => <div>Change password will be here</div>}/>      
+           <Route path='/forgotpassword' render={() => <div><ForgotPasswordForm/></div>}/>
+          <Route path='/newpassword' render={() => <div><NewPassword/></div>}/>
           <Route path='/test' render={() => <div>View custom components will be here</div>}/>      
           <Route path='*' render={() => <div>404 Not Found</div>}/>    
         </Switch>     
